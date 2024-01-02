22d99716fd65b72cb58d14f225784064fe82c05f
Minh Ta <minhvinhta@yahoo.com>
2024-01-02 18:03:48 +1000

    07. Implement a Root-Level Request Logger Middleware

--- myApp.js
+++ myApp.js
@@ -6,6 +6,11 @@ require('dotenv').config();
 
 app.use('/public', express.static(__dirname + '/public'));
 
+app.use((req, res, next) => {
+ console.log(`${req.method} ${req.path} - ${req.ip}`);
+ next();
+});
+
 // app.get('/', (req, res) => {
 //  res.send('Hello Express');
 // });
@@ -21,11 +26,6 @@ app.get('/json', (req, res) => {
  }
  res.json({'message': message});
 });
-
-
-
-
-
 
 
 
