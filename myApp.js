0991d316f7064a2fde2aee6bce5888b647fbbf3e
Minh Ta <minh.ta@creativeintersection.com>
2024-01-02 15:57:24 +1000

    03. Serve an HTML File

--- myApp.js
+++ myApp.js
@@ -1,15 +1,15 @@
 let express = require('express');
 let app = express();
 
-console.log('Hello World');
+// console.log('Hello World');
+
+// app.get('/', function (req, res) {
+//  res.send('Hello Express');
+// });
 
 app.get('/', function (req, res) {
- res.send('Hello Express');
+ res.sendFile(__dirname + '/views/index.html');
 });
-
-
-
-
 
 
 
