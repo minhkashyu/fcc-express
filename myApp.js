746dd38537884bdc09096b6e71ba81f32d396b94
Minh Ta <minhvinhta@yahoo.com>
2024-01-02 18:12:19 +1000

    08. Chain Middleware to Create a Time Server

--- myApp.js
+++ myApp.js
@@ -24,15 +24,15 @@ app.get('/json', (req, res) => {
  if (process.env.MESSAGE_STYLE === 'uppercase') {
   message = message.toUpperCase();
  }
- res.json({'message': message});
+ res.json({message: message});
 });
 
-
-
-
-
-
-
+app.get('/now', (req, res, next) => {
+ req.time = new Date().toString();
+ next();
+}, (req, res) => {
+ res.json({time: req.time});
+});
 
 
 
