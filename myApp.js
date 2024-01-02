8e55b66b68762ec7cff72a2289a6663796fe1110
Minh Ta <minhvinhta@yahoo.com>
2024-01-02 17:21:05 +1000

    05. Serve JSON on a Specific Route

--- myApp.js
+++ myApp.js
@@ -3,19 +3,19 @@ let app = express();
 
 // console.log('Hello World');
 
+app.use('/public', express.static(__dirname + '/public'));
+
 // app.get('/', function (req, res) {
 //  res.send('Hello Express');
 // });
-
-app.use('/public', express.static(__dirname + '/public'));
 
 app.get('/', function (req, res) {
  res.sendFile(__dirname + '/views/index.html');
 });
 
-
-
-
+app.get('/json', function (req, res) {
+ res.json({'message': 'Hello json'});
+});
 
 
 
