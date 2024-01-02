6a4d47bb4f5383091548878fa43c032608e9097e
Minh Ta <minhvinhta@yahoo.com>
2024-01-02 18:23:30 +1000

    09. Get Route Parameter Input from the Client

--- myApp.js
+++ myApp.js
@@ -34,9 +34,9 @@ app.get('/now', (req, res, next) => {
  res.json({time: req.time});
 });
 
-
-
-
+app.get('/:word/echo', (req, res) => {
+ res.json({echo: req.params.word});
+});
 
 
  module.exports = app;
