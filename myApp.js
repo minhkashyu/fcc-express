392164835fa697874c9aa97ff05f890429a6d487
Minh Ta <minhvinhta@yahoo.com>
2024-01-02 17:52:34 +1000

    06. Use the .env File * Convert functions to ES6 convention * Implement new config in .env

--- myApp.js
+++ myApp.js
@@ -1,26 +1,26 @@
 let express = require('express');
 let app = express();
+require('dotenv').config();
 
 // console.log('Hello World');
 
 app.use('/public', express.static(__dirname + '/public'));
 
-// app.get('/', function (req, res) {
+// app.get('/', (req, res) => {
 //  res.send('Hello Express');
 // });
 
-app.get('/', function (req, res) {
+app.get('/', (req, res) => {
  res.sendFile(__dirname + '/views/index.html');
 });
 
-app.get('/json', function (req, res) {
- res.json({'message': 'Hello json'});
+app.get('/json', (req, res) => {
+ let message = 'Hello json';
+ if (process.env.MESSAGE_STYLE === 'uppercase') {
+  message = message.toUpperCase();
+ }
+ res.json({'message': message});
 });
-
-
-
-
-
 
 
 
